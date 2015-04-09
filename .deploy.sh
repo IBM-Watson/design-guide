bba0f1989b2ebcc3b9524b021430e90939aae4d8
Sam Richard <snugug@gmail.com>
2015-04-09 10:30:59 -0400

    Let's see what the Branch is

--- .deploy.sh
+++ .deploy.sh
@@ -4,6 +4,8 @@
 ## Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 #########################
 
+echo "$TRAVIS_BRANCH"
+
 if [ "$TRAVIS_BRANCH" == "feature/travis" ]; then
   set -e # exit with nonzero exit code if anything fails
   echo "Conditional Test"
