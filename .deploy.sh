4cbba8d5aedb50b65b280c26f2d94197450c2e1a
Sam Richard <snugug@gmail.com>
2015-04-09 10:07:04 -0400

    Conditional Action Test

---
+++ .deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#########################
+## Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+#########################
+
+if [[ $TRAVIS_BRANCH == 'feature/travis' ]]
+  set -e # exit with nonzero exit code if anything fails
+  echo 'Conditional Test'
+fi
