2b6d3c7e915c8b1178e58783350f354b9df2eb95
Sam Richard <snugug@gmail.com>
2015-04-09 10:14:58 -0400

    Fix syntax error

--- .deploy.sh
+++ .deploy.sh
@@ -4,7 +4,7 @@
 ## Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 #########################
 
-if [[ $TRAVIS_BRANCH == 'feature/travis' ]]
+if [[ $TRAVIS_BRANCH == 'feature/travis' ]]; then
   set -e # exit with nonzero exit code if anything fails
   echo 'Conditional Test'
 fi
