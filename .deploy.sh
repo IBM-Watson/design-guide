c894c3777854269a3d0788760d682cdf66fc0c55
Sam Richard <snugug@gmail.com>
2015-04-28 19:59:11 -0400

    Move CNAME file

--- .deploy.sh
+++ .deploy.sh
@@ -7,8 +7,9 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" = "master" ]
   then
-    # Copy goodies from .www to www (build folder)
-    cp -r .www/ www/
+    # Copy CNAME from .www to www (build folder)
+    mv .www/CNAME www/CNAME
+
     # Move to build folder and init it
     cd www
     git init
